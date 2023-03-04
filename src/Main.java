9f670eec84d63a1b8dd6d673770d7cf548ed1b84
Alex.Sn <alex7777@nxt.ru>
2023-03-04 20:37:00 +0300

    change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Стоимость билета
         int amountTicket = 1500;
-        // Числовое значение, равное 1 мили
         int amountRub = 20;
         System.out.println("Вам начислено миль:");
-        int result;
-            result = amountTicket / amountRub;
+        int result = amountTicket / amountRub;
         System.out.println(result);
     }
 }
