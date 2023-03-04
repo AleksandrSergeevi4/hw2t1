97344d161d431bcaf9ab0fcf86b1f4a957781089
Alex.Sn <alex7777@nxt.ru>
2023-03-04 13:24:42 +0300

    new add files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Стоимость билета
+        int a = 1500;
+        // Числовое значение, равное 1 мили
+        int b = 20;
+        System.out.println("Вам начислено миль:");
+        int c = a / b;
+        System.out.println(c);
+    }
+}
